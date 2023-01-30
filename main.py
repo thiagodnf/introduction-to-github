7c917bc64d084946c47ac6c6d92ecde02567d9d2
Thiago Ferreira <thiagodnf@gmail.com>
2023-01-30 16:50:03 -0500

    deleted everything

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
 print('Hello World')
-print("My name is THIAGO")
-print("what is your name?")
-print('My name is Corbin!')
-print('My name is Ashton! ')
-<<<<<<< HEAD
-print('My name is Chris')
-print('My name is Justin!')
-=======
-print('My name is PEtER!')
->>>>>>> 93edc8f69351da1bc7e63958d95bd76d7d1e259f
